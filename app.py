52c048b7ff7ebc66a8c2973c9c058bdcf64e8d8b
Vinod5674 <vinodnishad7251@gmail.com>
2026-07-31 00:40:05 +0530

    Fix nltk deployment issue

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
 from flask import Flask, render_template, request
+import nltk
+import os
 
 from src.predict import predict_sentiment
+
+
+# NLTK resources download for Render
+nltk_data_path = "/opt/render/nltk_data"
+
+if not os.path.exists(nltk_data_path):
+    os.makedirs(nltk_data_path)
+
+nltk.data.path.append(nltk_data_path)
+
+resources = [
+    "stopwords",
+    "punkt",
+    "wordnet",
+    "omw-1.4"
+]
+
+for resource in resources:
+    try:
+        nltk.data.find(resource)
+    except LookupError:
+        nltk.download(resource, download_dir=nltk_data_path)
+
 
 app = Flask(__name__)
 
@@ -30,4 +55,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=5000)
